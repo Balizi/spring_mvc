e69f0d06286665ed40f52ac6e6265da1b4558638
Mohamed <mrbalizi52@gmail.com>
2022-12-01 10:15:20 +0100

    test function

--- src/main/java/com/example/demo1/HomeController.java
+++ src/main/java/com/example/demo1/HomeController.java
@@ -3,11 +3,14 @@ package com.example.demo1;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/show")
-    public String show(){
+    @RequestMapping("/show/{name}")
+    public String show(@PathVariable String name){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject(name);
         return "show";
     }
 }
--- src/main/webapp/WEB-INF/views/show.jsp
+++ src/main/webapp/WEB-INF/views/show.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: YC
-  Date: 30/11/2022
-  Time: 20:57
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Show</title>
 </head>
 <body>
-
+    <h1>Hello ,${name}</h1>
 </body>
 </html>
